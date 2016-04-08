0d09feb49ce73b06b861d2fa31a269cb219de404
benny446 <321680967@qq.com>
2016-04-08 15:15:35 -0400

    This is a test8

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 cd ${WORKSPACE}/src
-docker build -t 192.168.0.201:80/python-redis-demo:${BUILD_NUMBER} .
+docker build -t 192.168.5.47:80/python-redis-demo:${BUILD_NUMBER} .
 
-docker push 192.168.0.201:80/python-redis-demo:${BUILD_NUMBER}
+docker push 192.168.5.47:80/python-redis-demo:${BUILD_NUMBER}
 cd ${WORKSPACE}/test-build
 
 sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
 sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
 chmod 777 ./rancher-compose
 
-./rancher-compose --access-key 4C2184EAA5287B4E2ADB --secret-key 5goidz54c49xJbGQRaCzSzn9VXqDtc8ohSsUFdyL -p python-redis-demo-build${BUILD_NUMBER} up -d
+./rancher-compose --access-key 21AE5E8252F12581FF10 --secret-key XMJoubynia2yP8f2sdEkg4EY74m4EHURQLSRYXsm -p python-redis-demo-build${BUILD_NUMBER} up -d
 
